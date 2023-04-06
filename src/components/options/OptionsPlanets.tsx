c90f40fee818ff42aa45f0f9e4559c736d88bdbe
Adrian Zegnalek <zegnalekadrianoutlook.com>
2023-04-06 17:02:26 +0200

    changed option placement

--- src/components/options/OptionsPlanets.tsx
+++ src/components/options/OptionsPlanets.tsx
@@ -8,8 +8,8 @@ const OptionsPlanets = () => {
       <option value={'1'}>{t('oPlanetsComplimentary')}</option>
       <option value={'2'}>{t('oPlanetsSplitComplimentary')}</option>
       <option value={'3'}>{t('oPlanetsTriad')}</option>
-      <option value={'4'}>{t('oPlanetsEarth')}</option>
-      <option value={'5'}>{t('oPlanetsCavity')}</option>
+      <option value={'4'}>{t('oPlanetsCavity')}</option>
+      <option value={'5'}>{t('oPlanetsEarth')}</option>
       <option value={'6'}>{t('oPlanetsElemental')}</option>
     </>
   );
